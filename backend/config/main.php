124ce97f8330f33d2d04d635bab8c98165f0c3bf
Ronal Gorba Timothy <rg.timothy@icloud.com>
2020-11-22 15:53:41 +0700

    bug fix "as access" configuration

--- backend/config/main.php
+++ backend/config/main.php
@@ -27,10 +27,10 @@ return [
                 'assignment' => [
                     'class' => 'mdm\admin\controllers\AssignmentController',
                      // 'userClassName' => 'app\models\User', 
-                    'idField' => 'user_id',
+                    'idField' => 'id',
                     'usernameField' => 'username',
-                    'fullnameField' => 'profile.full_name',
-                    'extraColumns' => [
+                    // 'fullnameField' => 'profile.full_name',
+                    /*'extraColumns' => [
                         [
                             'attribute' => 'full_name',
                             'label' => 'Full Name',
@@ -52,8 +52,8 @@ return [
                                 return $model->profile->post->name;
                             },
                         ],
-                    ],
-                    'searchClass' => 'app\models\UserSearch'
+                    ],*/
+                    // 'searchClass' => 'app\models\UserSearch'
                 ],
                 /*'other' => [
                     'class' => 'path\to\OtherController', // add another controller
@@ -62,20 +62,8 @@ return [
         ],
     ],
     'components' => [
-        'as access' => [
-            'class' => 'mdm\admin\components\AccessControl',
-            'allowActions' => [
-                'site/*',
-                'admin/*',
-                // 'some-controller/some-action',
-                // The actions listed here will be allowed to everyone including guests.
-                // So, 'admin/*' should not appear here in the production, of course.
-                // But in the earlier stages of your development, you may probably want to
-                // add a lot of actions here until you finally completed setting up rbac,
-                // otherwise you may not even take a first step.
-            ]
-        ],
         'authManager' => [
+            'defaultRoles' => ['Guest'],
             'class' => 'yii\rbac\DbManager', // use 'yii\rbac\DbManager' or 'yii\rbac\PhpManager'
         ],
         'request' => [
@@ -114,6 +102,19 @@ return [
             ],
         ],
         */
+    ],
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            // 'site/*',
+            // 'admin/*',
+            // 'some-controller/some-action',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ],
     ],
     'params' => $params,
 ];
